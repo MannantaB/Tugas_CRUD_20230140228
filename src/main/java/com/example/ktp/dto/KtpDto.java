bef8a7b927cf4a4d8f0d613d78f1c55eacebbb21
MannantaB <mannanta18@gmail.com>
2026-03-29 21:05:12 +0700

    feat: add KtpDto for data transfer

--- src/main/java/com/example/ktp/dto/KtpDto.java
+++ src/main/java/com/example/ktp/dto/KtpDto.java
@@ -1,4 +1,12 @@
 package com.example.ktp.dto;
 
+import lombok.Data;
+
+@Data
 public class KtpDto {
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private String tanggalLahir;
+    private String jenisKelamin;
 }
