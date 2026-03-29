abe4d222129dfe0d0ae651cc3ef2a0504393b3f4
MannantaB <mannanta18@gmail.com>
2026-03-29 20:55:16 +0700

    feat: add dependency injection for KtpService in KtpController

--- src/main/java/com/example/ktp/controller/KtpController.java
+++ src/main/java/com/example/ktp/controller/KtpController.java
@@ -1,5 +1,6 @@
 package com.example.ktp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,5 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/ktp")
 @CrossOrigin(origins = "*")
+
 public class KtpController {
+
+    @Autowired
+    private KtpService service;
 }
