248bd81edcb1ea8df252e108c78e04d246e4b239
MannantaB <mannanta18@gmail.com>
2026-03-29 21:15:59 +0700

    feat: add KtpRepository with existsByNomorKtp validation

--- src/main/java/com/example/ktp/repository/KtpRepository.java
+++ src/main/java/com/example/ktp/repository/KtpRepository.java
@@ -1,4 +1,10 @@
 package com.example.ktp.repository;
 
-public interface KtpRepository {
+import com.example.ktp.entity.KtpEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface KtpRepository extends JpaRepository<KtpEntity, Integer> {
+    boolean existsByNomorKtp(String nomorKtp);
 }
