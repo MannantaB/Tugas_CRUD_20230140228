0a8baa78cc66fb7223125217199aaef5ffdd4ffa
MannantaB <mannanta18@gmail.com>
2026-03-29 21:20:39 +0700

    feat: define KtpService interface for CRUD operations

--- src/main/java/com/example/ktp/service/KtpService.java
+++ src/main/java/com/example/ktp/service/KtpService.java
@@ -1,4 +1,14 @@
 package com.example.ktp.service;
 
+import com.example.ktp.dto.KtpDto;
+import com.example.ktp.entity.KtpEntity;
+
+import java.util.List;
+
 public interface KtpService {
+    KtpEntity create(KtpDto dto);
+    List<KtpEntity> getAll();
+    KtpEntity getById(Integer id);
+    KtpEntity update(Integer id, KtpDto dto);
+    void delete(Integer id);
 }
